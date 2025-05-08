56de6c14f77b61a76e92979e1bce528d028a6d6a
mstrXxX007 <abdelhadi.bech@gmail.com>
2025-05-08 17:53:07 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Simple slider to cycle images
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Change slide every 3 seconds
+showSlide(currentSlide); // Initial call to show the first slide
